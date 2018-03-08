19d1f06eacff12f6951a5b1a5d1a6671650e381b
BXL078G <Brian_Langford@HomeDepot.com>
2018-03-08 10:38:08 -0600

    Add album and track interfaces

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http, Response } from '@angular/http';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
@@ -8,9 +10,9 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json';
 
-  getAlbum(id: number){
+  getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl)
-    .map(response => response.json());
+    .map(response => <Album>response.json());
   }
 
 }
