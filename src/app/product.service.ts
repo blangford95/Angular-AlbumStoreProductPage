0714442e44fc64a128d4f253b963dd223e3d336d
BXL078G <Brian_Langford@HomeDepot.com>
2018-03-08 11:10:18 -0600

    Add product list component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http, Response } from '@angular/http';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
@@ -9,10 +10,16 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl)
     .map(response => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl)
+    .map(response => <Product[]>response.json());
   }
 
 }
